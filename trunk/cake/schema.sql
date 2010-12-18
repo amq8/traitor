150da3c4f77a34fd639b00fd2b60b3b09203fd06
chorlick <chorlick@1821ebcc-768c-973b-927e-a1ffa1900dc7>
2010-12-18 23:28:42 +0000

    Added some directories for the daemon. And begin building the database.

--- trunk/cake/schema.sql
+++ trunk/cake/schema.sql
@@ -70,6 +70,28 @@ INSERT INTO `quotes` VALUES ('4d0c74f0-22f8-419d-a6aa-5bc78d6a6177','2010-12-17'
 UNLOCK TABLES;
 
 --
+-- Table structure for table `stocks`
+--
+
+DROP TABLE IF EXISTS `stocks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stocks` (
+  `uuid` binary(36) NOT NULL,
+  `symbol` varchar(32) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stocks`
+--
+
+LOCK TABLES `stocks` WRITE;
+/*!40000 ALTER TABLE `stocks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stocks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tools`
 --
 
@@ -128,4 +150,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-12-18  4:12:57
+-- Dump completed on 2010-12-18 17:27:02
